13496ec64eb28a2788a1e29ac3a00c5d5d1656e9
gary <huang20021@126.com>
2017-09-21 13:28:14 +0800

    spring session project init

---
+++ spring.session/src/main/java/com/gary/spring/session/App.java
@@ -0,0 +1,13 @@
+package com.gary.spring.session;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
